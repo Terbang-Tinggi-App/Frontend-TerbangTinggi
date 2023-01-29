c5965ac09b763c709b0446d9e8b23b4c2383672c
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2023-01-29 23:42:09 +0800

    chore: remove unused code

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import airportReducer from '../redux/airport/airport.slice';
 import authReducer from '../features/auth/redux/auth.slice';
 import counterReducer from '../redux/counter/counter.slice';
-import userReducer from '../redux/user/user.slice';
 import ticketReducer from '../redux/ticket/ticket.slice';
 import transactionsReducer from '../redux/transactions/transactions.slice';
 import { setAirport } from '../redux/airport/airport.actions';
@@ -18,8 +17,7 @@ export const store = configureStore({
     airport: airportReducer,
     counter: counterReducer,
     ticket: ticketReducer,
-    transactions: transactionsReducer,
-    user: userReducer
+    transactions: transactionsReducer
   }
 });
 
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,12 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      }
+    ]
   },
   plugins: [react()]
 });
